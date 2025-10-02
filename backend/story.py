419b7bbb45e6bd94ec37acb771cef4be88ae063c
zadam <zerenadam7@gmail.com>
2025-10-02 18:09:41 -0400

    Added Post class for story scraper, allows attributes to assigned variable.

--- backend/story.py
+++ backend/story.py
@@ -2,40 +2,32 @@ import requests
 from fake_useragent import UserAgent
 
 
-def getPosts(url, story_count=1):
+class Post:
+
+    def __init__(self, title, text, url):
+        self.title = title
+        self.text = text
+        self.url = url
+
+
+def GetPosts(url, story_count=1):
 
     if ".json" not in url:
         url = url + ".json"
 
     data = requests.get(url, headers={"User-Agent": UserAgent().chrome}).json()
 
-
     results = []
+
     for story in range(story_count):
-        post = data["data"]['children'][story]['data']
+        
+        post = data['data']['children'][story]['data']
         title = post['title']
         text = post['selftext']
         postURL = post['url']
 
-        results.append(
-            {
-                "title": title,
-                "text": text,
-                "URL": postURL
-            }
-        )
+        results.append(Post(title, text, postURL))
+
     return results
-
-
-
-
-
-
-
-    
-
-
-
-
 
 
