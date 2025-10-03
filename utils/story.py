b954b2b0a5446f84260de036c3e24d98aeb0a521
zadam <zerenadam7@gmail.com>
2025-10-03 00:59:17 -0400

    Tried implementing openai for filtering, cost money though. Updated a little code and created filter func

--- utils/story.py
+++ utils/story.py
@@ -1,6 +1,6 @@
 import requests
 from fake_useragent import UserAgent
-
+import os
 
 class Post:
 
@@ -14,28 +14,30 @@ class Scraper:
 
     def __init__(self):
         self.ua = UserAgent().chrome
-
+        
 
     def get(self, url, story_count=1):
 
         if ".json" not in url:
             url += ".json"
 
-        data = requests.get(url, headers=self.ua).json()
+        data = requests.get(url, headers={"User-Agent": self.ua}).json()
 
         results = []
 
-        while story_count <= 25: # max per request 
+        for story in range(story_count): # max per request 
         
-            post = data['data']['children'][story_count]['data']
+            post = data['data']['children'][story]['data']
             title = post['title']
             text = post['selftext']
             postURL = post['url']
 
             results.append(Post(title, text, postURL))
-            story_count += 1 
 
         return results
     
+    def filter(self, text):
+        pass
+
 
 
